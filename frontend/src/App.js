0e98a65e7c233ddbfee1bec7a4c22d1ca8364dc8
Jonathon Knack <jknack@mail.sfsu.edu>
2020-01-06 15:15:25 -0800

    landing page done...for NOW

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import LandingPage from './components/LandingPage'
+import SignIn from './components/SignIn'
+import Survey from './components/Survey'
+import PlaceOrder from './components/PlaceOrder'
+import './App.css'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Switch>
+        <Route path='/' exact component={LandingPage} />
+        <Route path='/signin' component={SignIn} />
+        <Route path='/survey' component={Survey} />
+        <Route path='/placeorder' component={PlaceOrder} />
+      </Switch>
+    </Router>
+  ) 
 }
 
 export default App;
