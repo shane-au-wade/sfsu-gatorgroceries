429249c7b5dc0c40f29ff358ec2a25b7d4eef8ee
Jonathon Knack <jknack@mail.sfsu.edu>
2020-01-09 17:25:12 -0800

    finished the sign in page, need to add axios to make the request and also need to add check for sfsu email.

---
+++ frontend/src/components/LogoHeader.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Logo from '../images/logo.png'
+
+const LogoHeader = () => {
+  return (
+    <div className='logo-wrapper'>
+        <img src={Logo} className='logo' alt='logo' />
+      </div>
+  )
+}
+
+export default LogoHeader
