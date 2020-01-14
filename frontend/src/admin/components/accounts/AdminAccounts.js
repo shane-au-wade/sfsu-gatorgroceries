bd24e7819e0b315a0d46518112d37cb33e2b768b
Shan Wade <shane.au.wade@gmail.com>
2020-01-14 15:41:19 -0800

    updated adminAccounts the admin accounts page is nearly ready to recieve data from the DB to generate table entries.

--- frontend/src/admin/components/accounts/AdminAccounts.js
+++ frontend/src/admin/components/accounts/AdminAccounts.js
@@ -2,13 +2,175 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import AdminHeader from '../adminHeader/adminHeader.jsx'
 
+import './adminAccounts.css'
+
+import editIcon from '../../icons/edit-24px.svg';
 
 const AdminAccounts = () => {
 return (
-    <div>
+    <div className='adminAccounts'>
     <AdminHeader selected='Accounts' username='Admin T.'></AdminHeader>
         <div className='AdminContentArea'>
-        !!!code goes here!!!
+        <h3 className='text-centered padded'>Users</h3>
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Type</th>
+            </tr>
+            <tr>
+                <td>Horace T.</td>
+                <td>Sys Admin</td>
+               
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            <tr>
+                <td>Shane W.</td>
+                <td>Dev</td> 
+                <td>
+                <img src={editIcon} className='editIcon'></img>
+                </td>
+            </tr>
+            
+            </table>
+
+            <p className='text-centered checkin'>
+                <button>
+                    Create 
+                </button>
+            </p>  
+
         </div>
     </div>
 )};
