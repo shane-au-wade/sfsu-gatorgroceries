d96086f5bcb5e9ab46f50c9cbee3d3c061143c7b
Eduardo Ramos <ramosedward8@gmail.com>
2020-01-15 14:39:19 -0800

    updated create events page

--- frontend/src/components/AdminCreateEvent.js
+++ frontend/src/components/AdminCreateEvent.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom'
 import AdminHeader from './adminHeader/adminHeader.jsx'
 import '../Admin-create-event-style.css'
 
+
+
 const AdminCreateEvent = () => {
 
     const [eventTitle, setEventTitle] = useState('')
@@ -42,21 +44,75 @@ const AdminCreateEvent = () => {
       }
 
 return (
-    <div>
+    <div className='admin-create-events'>
         <AdminHeader selected='Create Event' username='Admin T.'></AdminHeader>
         <div className='AdminContentArea'>
             <div className='title-container'>
-                <h2>Create An Event</h2>
+                <h2>Create Event</h2>
+                <button type='text'>Preview</button>
+              <button type='text'>Publish</button>
             </div>
+
+          
+
+
             <div className='create-event-background'>
-                <form>
+            
+            <div>
 
-                </form>
+                  <form onSubmit={handleForm}>
+                            
 
+                              <div className='form-container'>
+                              <label for="event title" >Event Title </label>
+                             
+                                
+                              <input type='text' name="event title" value={eventTitle} onChange={handleEventTitleChange} data-check="{&quot;max_length&quot;:{&quot;args&quot;:[255],&quot;when&quot;:&quot;submit&quot;}}" id="id_group-details-name" maxlength="255" name="group-details-name" required />
+                             
+                              </div>
+
+                              <div className='form-container'>
+                              <label for="location" >Location</label>
+                                
+                              <input type='text' name="location" value={location} onChange={handleLocationChange} required />
+                              
+                              </div>
+
+                              <div className='form-container'>
+                              <label for="start date" >Start Date</label>
+                                
+                              <input type='text' name="start date" value={startDate} onChange={handleStartDateChange} required />
+                              
+                              </div>
+
+                              <div className='start-end-time'>
+                              <div className='form-container'>
+                              <label for="start time" >Start Time</label>
+                                
+                              <input type='text' name="start time" value={startTime} onChange={handleStartTimeChange} required />
+                              
+                              </div>
+                              <div className='form-container'>
+                              <label for="end date">End Date</label>
+                              
+                              <input type='text' name="end date" value={endDate} onChange={handleEndDateChange} required />
+                              
+                              </div>
+                              <div className='form-container'>
+                              <label for="end time">End Time</label>
+                                
+                              <input type='text' name="end time" value={endTime} onChange={handleEndTimeChange} required />
+                              
+                              </div>
+                              </div>
+                  </form>
+                </div>
             </div>
-        </div>
-    </div>
-    
+         </div>
+         </div>
+
+         
+            
 )};
 
 export default AdminCreateEvent 
