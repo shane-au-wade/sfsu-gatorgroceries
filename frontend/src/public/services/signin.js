94ecfc75c0f43f81c6ad89ec9e92089e78ca42a9
Shane Wade <shane.au.wade@gmail.com>
2020-01-25 14:18:00 -0800

    debugged signin students can no longer enter emails that are not @mail.sfsu.edu emails

--- frontend/src/public/services/signin.js
+++ frontend/src/public/services/signin.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
 const verifyStudent = (student) => {
-  const request =  axios.post('student/verify-student', student)
-  return request.then(response => response.data)
+    const request =  axios.post('student/verify-student', student)
+    return request.then(response => response.data)
 }
 
 export default {verifyStudent}
