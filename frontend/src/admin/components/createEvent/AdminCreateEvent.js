811543f63f674d3234c401f5e54f9d53b9f51498
Steve Tu <stevetu11@yahoo.com>
2020-05-31 00:51:27 -0700

    Page now has list adding and removing functionality
    Mobile view still broken and currently, items with same name and qty will have duplicate keys but removal still works.

--- frontend/src/admin/components/createEvent/AdminCreateEvent.js
+++ frontend/src/admin/components/createEvent/AdminCreateEvent.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import AdminHeader from '../adminHeader/adminHeader.jsx'
 import './style/Admin-create-event-style.css'
 import addItem from '../../icons/add_circle_outline-24px.svg'
@@ -17,6 +17,7 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
 import ListItemText from '@material-ui/core/ListItemText';
+import DeleteIcon from '@material-ui/icons/Delete';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,7 +26,7 @@ const useStyles = makeStyles((theme) => ({
     transform: 'translate(-50%, -50%)',
     boxShadow: '0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)',
     background: '#f3f3f3',
-    marginTop: '-5%',
+    margin: '40px auto',
   },
 
   card: {
@@ -36,7 +37,55 @@ const useStyles = makeStyles((theme) => ({
 
 const AdminCreateEvent = (props) => {
   const classes = useStyles()
-  console.log("Passed props: ", props)
+
+  const [itemName, setItemName] = useState('')
+  const [itemQTY, setItemQTY] = useState('')
+  const [itemList, setItemList] = useState([])
+
+  //console.log("Passed props: ", props)
+
+  const itemAdd = () => {
+    // This will make sure that a item doesn't get added if name and/or qty textboxes are empty.
+    if(itemName !== '' || itemQTY !== '')
+    {
+      // Attach a unique id to the item.
+      const itemLabel = `item-list-label-${itemName}-${itemQTY}`
+
+      // Add the new item to the itemList state and render the component again.
+      setItemList(itemList.concat({ name: itemName, qty: itemQTY, id: itemLabel }))
+    }
+
+    // Clear the item and qty textboxes.
+    clearItemText()
+  }
+
+  const itemDelete = (itemLabel) => {
+    console.log("itemLabel is: ", itemLabel)
+    var itemIndex = -1
+
+    // This line is very important. Make a new reference to the itemList via spread syntax 
+    // such that setItemList will be sure to rerender the list correctly.
+    var tempItemList = [...itemList]
+    
+    // Find the index in which the object's id matches the itemLabel.
+    tempItemList.forEach(function(item, index){
+      if(item.id === itemLabel){
+        itemIndex = index
+      }
+    })
+
+    // If the itemIndex is not -1, take out the element at the itemIndex and set the state to the new item list.
+    if(itemIndex !== -1){
+      tempItemList.splice(itemIndex, 1)
+      setItemList(tempItemList)
+    }
+  }
+
+  // Set the name and qty textboxes states back to empty.
+  const clearItemText = () => {
+    setItemName('')
+    setItemQTY('')
+  }
 
   return (
     <div>
@@ -50,26 +99,32 @@ const AdminCreateEvent = (props) => {
               <TextField id='event-location' label='Location' type='text' style={{ width: 400, marginTop: '1%' }} required />
               <TextField id='event-date' type='date' label='Date' InputLabelProps={{ shrink: true }} style={{ width: 250, marginTop: '5%' }} required />
               <br />
-              <TextField id='event-startTime' type='time' label='Start' defaultValue='0:00' InputLabelProps={{ shrink: true }} style={{ width: 110, marginTop: '5%' }} required />
-              <TextField id='event-endTime' type='time' label='End' defaultValue='0:00' InputLabelProps={{ shrink: true }} style={{ width: 110, marginLeft: '5%', marginTop: '5%' }} required />
+              <TextField id='event-startTime' type='time' label='Start' InputLabelProps={{ shrink: true }} style={{ width: 110, marginTop: '5%' }} required />
+              <TextField id='event-endTime' type='time' label='End' InputLabelProps={{ shrink: true }} style={{ width: 110, marginLeft: '5%', marginTop: '5%' }} required />
               <br />
-              <TextField id='event-items' type='text' label='Item' InputLabelProps={{ shrink: true }} style={{ width: 200, marginTop: '5%' }} />
-              <TextField id='event-qty' type='number' label='QTY' defaultValue='1' InputProps={{ inputProps: { min: 1 } }} InputLabelProps={{ shrink: true }} style={{ width: 50, marginLeft: '5%', marginTop: '5%' }} />
-              <IconButton variant='contained' id='event-addItem' style={{ marginLeft: '1%', marginTop: '7%' }}><AddShoppingCartIcon /></IconButton>
+              <TextField id='event-items' value={itemName} onChange={e => setItemName(e.target.value)} type='text' label='Item' InputLabelProps={{ shrink: true }} style={{ width: 200, marginTop: '5%' }} />
+              <TextField id='event-qty' value={itemQTY} onChange={e => setItemQTY(e.target.value)} type='number' label='QTY' InputProps={{ inputProps: { min: 1 } }} InputLabelProps={{ shrink: true }} style={{ width: 50, marginLeft: '5%', marginTop: '5%' }} />
+              <IconButton variant='contained' id='event-addItem' onClick={() => itemAdd()} style={{ marginLeft: '1%', marginTop: '7%' }}><AddShoppingCartIcon /></IconButton>
 
               <List subheader='Items in Cart'>
+                {itemList.map((item) => {
+                  const itemLabel = `item-list-label-${item.name}-${item.qty}`
+
+                  return (
+                    <ListItem key={itemLabel}>
+                      <ListItemText primary={`Name: ${item.name}, QTY: ${item.qty}`} />
+                      <ListItemSecondaryAction>
+                        <IconButton edge='end' aria-label='delete' onClick={() => {itemDelete(itemLabel)}}><DeleteIcon /></IconButton>
+                      </ListItemSecondaryAction>
+                    </ListItem>
+                  )
+                })}
 
               </List>
             </form>
-            
-
-          </CardContent>
-        
-        
-        
+          </CardContent>  
         </Card>
       </div>
-
     </div>
   )
 }
