b7c97044ed28c9e7e174d62df73e3336c4355bd8
Steve Tu <stevetu11@yahoo.com>
2020-05-31 23:05:32 -0700

    Time Block algorithm complete with minor bugs
    Currently works fine for normal operating hours. If you set starting time to PM and the ending time to AM, the algorithm breaks.

--- frontend/src/admin/components/createEvent/AdminCreateEvent.js
+++ frontend/src/admin/components/createEvent/AdminCreateEvent.js
@@ -69,6 +69,9 @@ const AdminCreateEvent = (props) => {
   const [itemQTY, setItemQTY] = useState('')
   const [itemList, setItemList] = useState([])
 
+  const [startTime, setStartTime] = useState('')
+  const [endTime, setEndTime] = useState('')
+
   //console.log("Passed props: ", props)
 
   const itemAdd = (e) => {
@@ -119,7 +122,241 @@ const AdminCreateEvent = (props) => {
 
   // Calculate time blocks and then push user to the preview screen.
   const calculateTimeBlocks = (e) => {
-    console.log("Contents: ", e)
+    e.preventDefault()
+
+    // Split the start and end times by the colon, seperating them into hours and minutes. Then create the start and end Date objects.
+    let startArr =  startTime.split(':');
+    let endArr =  endTime.split(':');
+
+    let timeBlocks = []
+
+    // Get individual hours and minutes from start and end arrays.
+    let startHour = startArr[0]
+    let endHour = endArr[0]
+    let startMinutes = startArr[1]
+    let endMinutes = endArr[1]
+
+    // Perform check to see if start and end hours are less than 10. If so, remove the zero in front of them.
+    if(startHour < 10){
+      startHour = startHour.substr(1)
+    }
+    if(endHour < 10){
+      endHour = endHour.substr(1)
+    }
+
+    // Perform check to see if the end hour is AM or PM.
+    let endTimeDesignation = parseInt(endHour)
+    if(endTimeDesignation > 11){
+      endTimeDesignation = "PM"
+    }
+    else if(endTimeDesignation === 24 && endTimeDesignation < 12){
+      endTimeDesignation = "AM"
+    }
+
+    // Calculate time block size from start to end.
+    let blockSize = Math.abs(endHour - startHour)
+
+    console.log("Block size between start and end is: ", blockSize)
+
+    // Determine whether the starting hour is AM or PM.
+    if(startHour > '11')
+    {
+      console.log("Starting time in PM")
+
+      let newStartHour = 0
+
+      // Calculate the next hour after the starting hour and set timeDesignation to the next hour with the minutes.
+      let nextHour = parseInt(startHour) + 1
+      let timeDesignation = [nextHour, startMinutes]
+
+      // If the start hour is after 12PM, convert it to standard. Otherwise, keep it the same.
+      if(startHour > 13){
+        newStartHour = Math.abs(startHour - 12)
+      }
+      else{
+        newStartHour = startHour
+      }
+
+      // If the next hour after the start hour is after 12PM, convert it to standard.
+      if(nextHour > 13){
+        nextHour = Math.abs(nextHour - 12)
+      }
+
+      // If the next hour is after 11:59AM, set timeDesignation to PM. Otherwise, set it to AM.
+      if(timeDesignation[0] > '11'){
+        timeDesignation = "PM"
+      }
+      else{
+        timeDesignation = "AM"
+      }
+
+      // Hardcoded to look for 13 to convert to 1 for the very first block to get rid of an annoying bug.
+      if(nextHour === 13){
+        nextHour = 1
+      }
+
+      // Push the first time block onto timeBlocks array and move to the for loop for the rest of the time blocks.
+      timeBlocks.push({ block: `${newStartHour}:${startMinutes}AM - ${nextHour}:${startMinutes}${timeDesignation}` })
+
+      // For loop through timeBlocks and push a block in specified format. If the end is reached, push the final block
+      // and set the end minutes.
+      for(var i = 1; i < blockSize; i++){
+        // Get the next hour and the hour after it.
+        nextHour = parseInt(startHour) + i
+        let nextNextHour = parseInt(startHour) + i + 1
+
+        // Set timeDesignations to the next hour and the hour after that.
+        let timeDesignations = [nextHour, nextNextHour]
+
+        // Determine the time designations for the next hour and the hour after that.
+        if(timeDesignations[0] > '11'){
+          timeDesignations[0] = "PM"
+        }
+        else{
+          timeDesignations[0] = "AM"
+        }
+        if(timeDesignations[1] > '11'){
+          timeDesignations[1] = "PM"
+        }
+        else{
+          timeDesignations[1] = "AM"
+        }
+
+        // Determine whether or not the next hour and the hour after that need to be converted to standard time.
+        if(nextHour >= 13){
+          nextHour = Math.abs(nextHour - 12)
+        }
+        if(nextNextHour >= 13){
+          nextNextHour = Math.abs(nextNextHour - 12)
+        }
+
+        // If this is not the last element in the timeBlocks, push to timeBlocks another block 
+        // with the next hour and the hour after that in the specified format.
+        if(i + 1 < blockSize){
+          timeBlocks.push({ block: `${nextHour}:${startMinutes}${timeDesignations[0]} - ${nextNextHour}:${startMinutes}${timeDesignations[1]}`})
+        }
+        else{
+          // If this is the last element, determine the final timeDesignation and then push onto timeBlocks the 2nd to last hour and then the end hour.
+          // Perform check to see if the end hour is AM or PM.
+          let timeDesignation = endHour
+          if(timeDesignation > '11'){
+            timeDesignation = "PM"
+          }
+          else{
+            timeDesignation = "AM"
+          }
+
+          // Perform check to see if the end hour needs to be converted to standard time.
+          if(endHour > 13){
+            endHour = Math.abs(endHour - 12)
+          }
+
+          timeBlocks.push({ block: `${nextHour}:${startMinutes}${timeDesignations[0]} - ${endHour}:${endMinutes}${timeDesignation}`})
+        }
+      }
+
+      console.log("Time blocks are: ", timeBlocks)
+    }
+    else{
+      console.log("Starting time in AM")
+
+      if(startHour < 10){
+        startHour = startHour.substr(1)
+      }
+
+      let newStartHour = 0
+
+      // Calculate time block size from start to end.
+      let blockSize = endHour - startHour
+      console.log("Block size between start and end is: ", blockSize)
+
+      // Calculate the next hour after the starting hour and set timeDesignation to the next hour with the minutes.
+      let nextHour = parseInt(startHour) + 1
+      let timeDesignation = [nextHour, startMinutes]
+
+      // If the start hour is after 12PM, convert it to standard. Otherwise, keep it the same.
+      if(startHour > 13){
+        newStartHour = Math.abs(startHour - 12)
+      }
+      else{
+        newStartHour = startHour
+      }
+
+      // If the next hour after the start hour is after 12PM, convert it to standard.
+      if(nextHour > 13){
+        Math.abs(nextHour - 12)
+      }
+
+      // If the next hour is after 11:59AM, set timeDesignation to PM. Otherwise, set it to AM.
+      if(timeDesignation[0] > '11'){
+        timeDesignation = "PM"
+      }
+      else{
+        timeDesignation = "AM"
+      }
+
+      // Push the first time block onto timeBlocks array and move to the for loop for the rest of the time blocks.
+      timeBlocks.push({ block: `${newStartHour}:${startMinutes}AM - ${nextHour}:${startMinutes}${timeDesignation}` })
+
+      // For loop through timeBlocks and push a block in specified format. If the end is reached, push the final block
+      // and set the end minutes.
+      for(var i = 1; i < blockSize; i++){
+        // Get the next hour and the hour after it.
+        nextHour = parseInt(startHour) + i
+        let nextNextHour = parseInt(startHour) + i + 1
+
+        // Set timeDesignations to the next hour and the hour after that.
+        let timeDesignations = [nextHour, nextNextHour]
+
+        // Determine the time designations for the next hour and the hour after that.
+        if(timeDesignations[0] > '11'){
+          timeDesignations[0] = "PM"
+        }
+        else{
+          timeDesignations[0] = "AM"
+        }
+        if(timeDesignations[1] > '11'){
+          timeDesignations[1] = "PM"
+        }
+        else{
+          timeDesignations[1] = "AM"
+        }
+
+        // Determine whether or not the next hour and the hour after that need to be converted to standard time.
+        if(nextHour >= 13){
+          nextHour = Math.abs(nextHour - 12)
+        }
+        if(nextNextHour >= 13){
+          nextNextHour = Math.abs(nextNextHour - 12)
+        }
+
+        // If this is not the last element in the timeBlocks, push to timeBlocks another block 
+        // with the next hour and the hour after that in the specified format.
+        if(i + 1 < blockSize){
+          timeBlocks.push({ block: `${nextHour}:${startMinutes}${timeDesignations[0]} - ${nextNextHour}:${startMinutes}${timeDesignations[1]}`})
+        }
+        else{
+          // If this is the last element, determine the final timeDesignation and then push onto timeBlocks the 2nd to last hour and then the end hour.
+          // Perform check to see if the end hour is AM or PM.
+          let timeDesignation = endHour
+          if(timeDesignation > '11'){
+            timeDesignation = "PM"
+          }
+          else{
+            timeDesignation = "AM"
+          }
+
+          // Perform check to see if the end hour needs to be converted to standard time.
+          if(endHour > 13){
+            endHour = Math.abs(endHour - 12)
+          }
+
+          timeBlocks.push({ block: `${nextHour}:${startMinutes}${timeDesignations[0]} - ${endHour}:${endMinutes}${timeDesignation}`})
+        }
+      }
+
+      console.log("Time blocks are: ", timeBlocks)
+    }
   }
 
   return (
@@ -134,13 +371,13 @@ const AdminCreateEvent = (props) => {
               <TextField id='event-location' label='Location' type='text' style={{}} required />
               <TextField id='event-date' type='date' label='Date' InputLabelProps={{ shrink: true }} style={{ width: 250, marginTop: '5%' }} required />
               <br />
-              <TextField id='event-startTime' type='time' label='Start' InputLabelProps={{ shrink: true }} style={{ width: 110, marginTop: '5%' }} required />
-              <TextField id='event-endTime' type='time' label='End' InputLabelProps={{ shrink: true }} style={{ width: 110, marginLeft: '5%', marginTop: '5%' }} required />
+              <TextField id='event-startTime' type='time' label='Start' onChange={e => setStartTime(e.target.value)} InputLabelProps={{ shrink: true }} style={{ width: 110, marginTop: '5%' }} required />
+              <TextField id='event-endTime' type='time' label='End' onChange={e => setEndTime(e.target.value)} InputLabelProps={{ shrink: true }} style={{ width: 110, marginLeft: '5%', marginTop: '5%' }} required />
               <br />
               
               <form onSubmit={(e) => itemAdd(e)}>
-                <TextField id='event-items' value={itemName} onChange={e => setItemName(e.target.value)} type='text' label='Item' InputLabelProps={{ shrink: true }} style={{ width: 200, marginTop: '5%' }} required/>
-                <TextField id='event-qty' value={itemQTY} onChange={e => setItemQTY(e.target.value)} type='number' label='QTY' InputProps={{ inputProps: { min: 1 } }} InputLabelProps={{ shrink: true }} style={{ width: 50, marginLeft: '5%', marginTop: '5%' }} required/>
+                <TextField id='event-items' value={itemName} onChange={e => setItemName(e.target.value)} type='text' label='Item' InputLabelProps={{ shrink: true }} style={{ width: 200, marginTop: '5%' }} />
+                <TextField id='event-qty' value={itemQTY} onChange={e => setItemQTY(e.target.value)} type='number' label='QTY' InputProps={{ inputProps: { min: 1 } }} InputLabelProps={{ shrink: true }} style={{ width: 50, marginLeft: '5%', marginTop: '5%' }} />
                 <IconButton variant='contained' id='event-addItem' type='submit' style={{ marginLeft: '5%', marginTop: '7%' }}><AddShoppingCartIcon /></IconButton>
               </form>
 
