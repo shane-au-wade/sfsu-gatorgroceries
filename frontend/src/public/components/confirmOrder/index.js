3c19db852236f93d318b92e7238c4db53a0475c4
Shane Wade <shane.au.wade@gmail.com>
2020-06-01 14:25:36 -0700

    Frontend changes for confirmed page

--- frontend/src/public/components/confirmOrder/index.js
+++ frontend/src/public/components/confirmOrder/index.js
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style/confirmOrder.css'
 import logo from '../../images/logo.png'
+import axios from 'axios'
 import completedLogo from '../../images/completed-order.png'
-import checkEmail from '../../images/check_email.svg'
 
-const CompletedOrder = (props) => {
 
-  console.log('order confirmed props', props)
+const ConfirmOrder = (props) => {
+
+  console.log('order confirmed props', props.match.params)
 
   // use the use effect to query to DB to ensure that the order
   // is valid and check if it has already been confirmed. 
+
+  const [confirmMsg, setConfirmMsg] = useState('Confirming...')
+
+  useEffect(() => {
+   
+    //axios query to orders/confirm order with body data, 
+    
+
+   axios.post('/orders/confirm-order', props.match.params).then(order => {
+
+    console.log(order.data.confirmed)
+      if(order.data.confirmed)
+      {
+        setConfirmMsg('Your order is confirmed! See you soon')
+      }
+   }).catch(err => {
+     console.log(err)
+   })
+  
+    
+
+
+  }, [confirmMsg]); 
 
   const handleSubmit = (event) =>{
     event.preventDefault();
@@ -28,16 +52,15 @@ const CompletedOrder = (props) => {
       </div>
       <div className='spacer'></div>
       <div className='centered-container'>  
-      <p> Click the button below to confirm your order </p>
-      <div className='submit-place-order'>
-                <button type='submit' id='place-order-submit' onClick={handleSubmit}>Confirm Order</button>
-            </div>
+      <p> {confirmMsg} </p>
+      <p>
+      <img src={completedLogo} alt='logo' className='main-logo'></img>
+      </p>
             
        </div>
       </div>
             
-     
   )
 }
 
-export default CompletedOrder
+export default ConfirmOrder
