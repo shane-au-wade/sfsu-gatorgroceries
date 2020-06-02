5d444a43608079bd67c5b1b169cd848c70a0a1a1
Shane Wade <shane.au.wade@gmail.com>
2020-06-01 18:06:52 -0700

    updated build for deploy

--- backend/build/service-worker.js
+++ backend/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8458812a86a288ab1f2e552300e7359a.js"
+  "/precache-manifest.8448868677e602b655a5ad5c5a1d7ef2.js"
 );
 
 self.addEventListener('message', (event) => {
